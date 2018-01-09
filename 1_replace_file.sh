0c2d759c855a5270169bd6ed371ca368b2609493
brianlee <brian50208lee@gmail.com>
2018-01-10 00:27:18 +0800

    update fps

--- 1_replace_file.sh
+++ 1_replace_file.sh
@@ -1,3 +1,4 @@
 cp -f LF2_agent/replace/game.html F.LF/game/game.html
 cp -f LF2_agent/replace/manager.js F.LF/LF/manager.js
 cp -f LF2_agent/replace/Crusher.js LF2_19/AI/Crusher.js
+cp -f LF2_agent/replace/global.js F.LF/LF/global.js
