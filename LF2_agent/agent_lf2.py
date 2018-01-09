a8e55477bbdd38c9fdca4bf09cdf86685fdce788
brianlee <brian50208lee@gmail.com>
2018-01-09 22:31:21 +0800

    add prepro

--- LF2_agent/agent_lf2.py
+++ LF2_agent/agent_lf2.py
@@ -2,6 +2,7 @@ import random
 import logging
 from flask import Flask, request
 from flask_cors import CORS
+import numpy as np
 
 # init
 app = Flask(__name__)
@@ -11,14 +12,31 @@ CORS(app, supports_credentials=True)
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+# np print option
+np.set_printoptions(precision=2)
+
 # load model
 model = None
+
+def prepro(observation):
+    def one_hot(idx, length):
+        vector = [0.0]*length
+        vector[idx] = 1.0
+        return vector
+    observation[4] = 1.0 if observation[4] == 'true' else 0.0
+    observation[9] = 1.0 if observation[9] == 'true' else 0.0
+    observation[10:11] = one_hot(int(observation[10]), 17)
+    observation[5:6] = one_hot(int(observation[5]), 17)
+    observation = np.array(observation, dtype='float32')
+    return observation
 
 @app.route("/agent_lf2/choose_action")
 def choose_action():
     observation = request.args.get('observation').split(',')
     print('====================  choose_action  ====================')
     print('observation:', observation)
+    # process
+    observation = prepro(observation)
     action = int(random.random()*8)
     return str(action)
 
@@ -35,6 +53,9 @@ def store_transition():
     print('reward:     ', reward)
     print('observation:', observation)
     print('done:       ', done)
+    # process
+    pre_observation = prepro(pre_observation)
+    observation = prepro(observation)
     return 'success'
 
 if __name__ == "__main__":
